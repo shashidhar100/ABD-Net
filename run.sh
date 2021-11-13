b28838e60908ceddc20d54db268dc340c3031d79
SHASHIDHAR VEERAPPA KUDARI <51244857+shashidhar100@users.noreply.github.com>
2021-11-13 20:53:03 +0530

    Delete run.sh
    deleting run.sh file

--- run.sh
+++
@@ -1,3 +0,0 @@
-conda activate torch
-python train.py --log_dir "decomposer_v1_1024_max_NT" --batch_size 16 --normals True --epoch 50
-# python train.py --log_dir "decomposer_v1_1024_attn_NF" --batch_size 16 --epoch 50
